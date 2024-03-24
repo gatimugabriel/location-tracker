77a84d8c5059accca2b6bb4dbef6aa2b0a30dea7
Gabriel Gerald <geraldg652@gmail.com>
2024-03-24 16:42:44 +0300

    test: dep

--- client/src/hooks/useMap.js
+++ client/src/hooks/useMap.js
@@ -2,106 +2,104 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const useMap = () => {
-    const [position, setPosition] = useState({
-        lat: 0,
-        lng: 0
+    const [currentPosition, setCurrentPosition] = useState({
+        lat: 0, lng: 0
     })
 
-    const [lastRecordedPosition, setLastRecordedPosition] = useState(null)
-    const [dwellTimeoutId, setDwellTimeoutId] = useState(null)
-    const dwellTime = 1000 * 60   // 1 minute (testing purpose)
+    const [lastPosition, setLastPosition] = useState(null)
+    const [timeoutId, setTimeoutId] = useState(null)
+    const timeoutDuration = 1000 * 3 // 10 ms for testing
 
-    const getCurrentLocation = () => {
-        navigator.geolocation.getCurrentPosition(
-            ({coords}) => {
-                setPosition({lat: coords.latitude, lng: coords.longitude});
-            },
-            (blocked) => {
-                if (blocked) {
-                    const fetch = async () => {
-                        try {
-                            const {data} = await axios.get("https://ipapi.co/json");
-                            setPosition({lat: data.latitude, lng: data.longitude});
-                        } catch (err) {
-                            console.error(err);
-                        }
-                    };
-                    fetch();
-                }
+    const fetchCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition(({coords}) => {
+            setCurrentPosition({lat: coords.latitude, lng: coords.longitude});
+        }, (locationBlocked) => {
+            if (locationBlocked) {
+                const fetchLocation = async () => {
+                    try {
+                        const {data} = await axios.get("https://ipapi.co/json");
+                        setCurrentPosition({lat: data.latitude, lng: data.longitude});
+                    } catch (err) {
+                        console.error(err);
+                    }
+                };
+                fetchLocation();
             }
-        );
+        });
     }
 
     useEffect(() => {
-       getCurrentLocation()
+        fetchCurrentLocation()
 
-        const watchId = navigator.geolocation.watchPosition(
-            async ({coords}) => {
-                const {latitude, longitude} = coords;
-                setPosition({lat: latitude, lng: longitude});
+        const locationWatchId = navigator.geolocation.watchPosition(async ({coords}) => {
+            const {latitude, longitude} = coords;
+            setCurrentPosition({lat: latitude, lng: longitude});
 
-                if (lastRecordedPosition === null ||
-                    hasTraveledSignificantDistance(
-                        lastRecordedPosition,
-                        {latitude, longitude},
-                        1000 // 1 km
-                    )
-                ) {
-                    clearTimeout(dwellTimeoutId)
-                    const newDwellTimeoutId = setTimeout(() => {
-                        sendLocationToBackend({latitude, longitude})
-                        setLastRecordedPosition({latitude, longitude})
-                    }, dwellTime)
-                    setDwellTimeoutId(newDwellTimeoutId)
-                }
+            if (lastPosition === null || hasSignificantDistanceTravelled(lastPosition, {latitude, longitude}, 1)) {
+                clearTimeout(timeoutId)
+                const newTimeoutId = setTimeout(async () => {
 
-                // sendLocationToBackend
-                await sendLocationToBackend({latitude, longitude})
-
-            }, (error) => {
-                console.error(error)
-            })
+                    // Check if the new location is the same as the last recorded location
+                    console.log('lastPosition', lastPosition)
+                    if (lastPosition && lastPosition.latitude === latitude && lastPosition.longitude === longitude) {
+                        console.log('User still in the same location. No need to send location to server.');
+                    } else {
+                        console.log('send coordinates')
+                        await sendLocationToServer({latitude, longitude})
+                        setLastPosition({latitude, longitude})
+                    }
+                }, timeoutDuration)
+                setTimeoutId(newTimeoutId)
+            }
+        }, (error) => {
+            console.error(error)
+        })
 
         // cleanup
         return () => {
-           navigator.geolocation.clearWatch(watchId)
-       }
-    }, []);
+            navigator.geolocation.clearWatch(locationWatchId)
+        }
+    }, [lastPosition]);
 
+    const hasSignificantDistanceTravelled = ({latitude: lat1, longitude: lng1}, {
+        latitude: lat2,
+        longitude: lng2
+    }, minDistance) => {
+        const earthRadiusInMetres = 6371e3; // Earth's radius in metres
 
-    const hasTraveledSignificantDistance = (
-        {latitude: lat1, longitude: lng1},
-        {latitude: lat2, longitude: lng2},
-        minDistance
-    ) => {
-        const R = 6371e3; // Earth's radius (metres)
-        const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
-        const φ2 = (lat2 * Math.PI) / 180;
-        const Δφ = ((lat2 - lat1) * Math.PI) / 180;
-        const Δλ = ((lng2 - lng1) * Math.PI) / 180;
+        // Convert latitudes and longitudes from degrees to radians
+        const lat1InRadians = (lat1 * Math.PI) / 180;
+        const lat2InRadians = (lat2 * Math.PI) / 180;
+        const deltaLatInRadians = ((lat2 - lat1) * Math.PI) / 180;
+        const deltaLngInRadians = ((lng2 - lng1) * Math.PI) / 180;
 
-        const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        // Haversine formula: a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+        const a = Math.sin(deltaLatInRadians / 2) * Math.sin(deltaLatInRadians / 2) + Math.cos(lat1InRadians) * Math.cos(lat2InRadians) * Math.sin(deltaLngInRadians / 2) * Math.sin(deltaLngInRadians / 2);
 
+        // Haversine formula: c = 2 ⋅ atan2( √a, √(1−a) )
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = R * c; // in metres
 
-        return distance >= minDistance;
+        // Haversine formula: d = R ⋅ c
+        const distanceInMetres = earthRadiusInMetres * c; // Distance in metres
+
+        console.log(minDistance, distanceInMetres)
+        console.log(distanceInMetres >= minDistance)
+        return distanceInMetres >= minDistance;
+
+        // return true
     }
 
-    const sendLocationToBackend = async ({latitude, longitude}) => {
+    const sendLocationToServer = async ({latitude, longitude}) => {
         try {
             const response = await axios.post("http://127.0.0.1:8080/api/v1/location", {
-                latitude,
-                longitude
+                latitude, longitude
             });
             console.log('Location recorded', response.data)
         } catch (err) {
-            console.error("Error sending location to backend");
+            console.error("Error sending location to server");
             console.error(err);
         }
     };
 
-    return {position};
+    return {position: currentPosition};
 };
