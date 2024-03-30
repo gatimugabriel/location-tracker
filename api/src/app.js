22cb3beb987be29459d989ffdcae0c63a48c54a6
Gabriel Gatimu <geraldg652@gmail.com>
2024-03-30 12:36:13 +0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -17,6 +17,7 @@ const corsOptions = {
     credentials: true,
 }
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
